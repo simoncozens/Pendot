ae882a8f3303f95989b04f8d70797726d824a90e
Simon Cozens <simon@simon-cozens.org>
2024-04-09 14:05:08 +0100

    Add guideline designer

--- Pendot Designer.py
+++ Pendot Designer.py
@@ -7,10 +7,18 @@ import vanilla
 from pathlib import Path
 from GlyphsApp import Glyphs, GSLayer, GSFontMaster, GSApplication
 import sys
+import AppKit
 
 sys.path.append(str(Path(__file__).parent.parent / "Plugins" / "Dotter"))
 
-from pendot import PARAMS, doStroker, KEY, doDotter, addComponentGlyph
+from pendot import (
+    PARAMS,
+    doStroker,
+    KEY,
+    doDotter,
+    addComponentGlyph,
+    add_guidelines_to_layer,
+)
 import traceback
 
 
@@ -18,6 +26,42 @@ PREVIEW_MASTER_NAME = "Pendot Preview"
 GSSteppingTextField = objc.lookUpClass("GSSteppingTextField")
 
 
+# Parameters for pendot are stored per instance.
+# Each instance has a default userdata entry for each parameter; for example,
+# instance.userData["co.uk.corvelsoftware.Dotter.dotSize"] stores the
+# dot size for this instance. However, each layer may provide an instance-specific
+# override, allowing you to have, for example, different dot sizes for the letter
+# B. This is stored in the layer.userData dictionary, but the key includes the
+# instance name; for example:
+# font.glyphs["B"].layers[0].userData["co.uk.corvelsoftware.Dotter.Regular.dotSize"]
+#
+# See pendot/dotter.py and pendot/stroker.py for the list of parameters, but
+# it will be something like:
+#     # Dotter parameters
+#     "dotSize": 15,
+#     "dotSpacing": 15,
+#     "preventOverlaps": True,
+#     "splitPaths": False,
+#     "contourSource": "<Default>",
+#     # Stroker parameters
+#     "strokerWidth": 50,
+#     "strokerHeight": 50,
+#     "strokerAngle": 0,
+#     "strokerHeightLock": True,
+#     "startCap": "round",
+#     "endCap": "round",
+#     "joinType": "round",
+#     "removeExternal": False,
+#     "removeInternal": False,
+#     "segmentWise": False
+
+# Guidelines parameters are stored in the instance as
+# instance.userData["co.uk.corvelsoftware.Dotter.guidelines"]. This is a list
+# of tuples, each of which is a pair of (height, thickness). The height is
+# either a number of font units, or one of the names of the Glyphs metrics.
+
+
+# Vanilla backports
 class SteppingTextBox(vanilla.EditText):
     nsTextFieldClass = GSSteppingTextField
 
@@ -38,6 +82,33 @@ class SteppingTextBox(vanilla.EditText):
         else:
             alpha = 0.2
         self._nsObject.setAlphaValue_(alpha)
+
+
+class ComboBoxList2Cell(vanilla.ComboBox):
+    def __init__(self, items=[], completes=True, editable=False, callback=None):
+        super().__init__(
+            posSize="auto",
+            items=items,
+            completes=completes,
+            continuous=False,
+            sizeStyle="small",
+            callback=callback,
+        )
+        self.enable(editable)
+
+
+class SteppingTextBoxList2Cell(SteppingTextBox):
+    def __init__(self, **kwargs):
+        # Force it to be editable
+        if "editable" in kwargs:
+            del kwargs["editable"]
+        super().__init__(
+            **kwargs,
+            posSize="auto",
+            sizeStyle="small",
+            readOnly=False,
+        )
+        self.enable(True)
 
 
 class LabelledComponent(vanilla.Group):
@@ -152,6 +223,9 @@ class OverridableComponent(vanilla.Group):
                 self.overridewidget.set(layers[0].userData[layer_instance_override])
 
 
+# On with the show.
+
+
 class PendotDesigner:
     def __init__(self):
         self.idempotence = False
@@ -165,7 +239,11 @@ class PendotDesigner:
             ),
         )
         self.widget_reloaders = []
-        self.w.tabs = vanilla.Tabs("auto", ["Dotter", "Stroker", "Guidelines"], callback=self.createLayerPreview)
+        self.w.tabs = vanilla.Tabs(
+            "auto",
+            ["Dotter", "Stroker", "Guidelines"],
+            callback=self.createLayerPreview,
+        )
         dotterTab, strokerTab, guidelineTab = self.w.tabs
 
         # Set up dotter tab
@@ -174,7 +252,13 @@ class PendotDesigner:
             basepos = (10, 30, -10, 30)
             for name, title, widget, args in controls:
                 component = OverridableComponent(
-                    self, name, basepos, title, widget, args, postChange=self.createLayerPreview
+                    self,
+                    name,
+                    basepos,
+                    title,
+                    widget,
+                    args,
+                    postChange=self.createLayerPreview,
                 )
                 setattr(tab, name, component)
                 self.widget_reloaders.append(component.loadValues)
@@ -202,20 +286,65 @@ class PendotDesigner:
                 ("strokerWidth", "Stroke Width", SteppingTextBox, {}),
                 ("strokerHeight", "Stroke Height", SteppingTextBox, {}),
                 ("strokerAngle", "Stroke Angle", SteppingTextBox, {}),
-                ("startCap", "StartCap", vanilla.PopUpButton, {"items": ["Round", "Circle", "Square"]}),
-                ("endCap", "EndCap", vanilla.PopUpButton, {"items": ["Round", "Circle", "Square"]}),
-                ("joinType", "Join Type", vanilla.PopUpButton, {"items": ["Round", "Bevel", "Mitre", "Circle"]}),
+                (
+                    "startCap",
+                    "StartCap",
+                    vanilla.PopUpButton,
+                    {"items": ["Round", "Circle", "Square"]},
+                ),
+                (
+                    "endCap",
+                    "EndCap",
+                    vanilla.PopUpButton,
+                    {"items": ["Round", "Circle", "Square"]},
+                ),
+                (
+                    "joinType",
+                    "Join Type",
+                    vanilla.PopUpButton,
+                    {"items": ["Round", "Bevel", "Mitre", "Circle"]},
+                ),
                 ("removeExternal", "Remove External", vanilla.CheckBox, {"title": ""}),
                 ("removeInternal", "Remove Internal", vanilla.CheckBox, {"title": ""}),
                 ("segmentWise", "Stroke Each Segment", vanilla.CheckBox, {"title": ""}),
             ],
         )
 
+        # Set up Guidelines tab
+        columnDescriptions = [
+            {
+                "identifier": "height",
+                "title": "Height",
+                "editable": True,
+                "cellClass": vanilla.ComboBoxList2Cell,
+                "cellClassArguments": {
+                    "items": [m.name for m in Glyphs.font.masters[0].metrics()]
+                },
+            },
+            {
+                "identifier": "thickness",
+                "title": "Thickness",
+                # "cellClass": SteppingTextBoxList2Cell,
+                "editable": True,
+            },
+        ]
+
+        guidelineTab.list = vanilla.List2(
+            "auto",
+            items=[],
+            columnDescriptions=columnDescriptions,
+            editCallback=self.editGuidelines,
+            deleteCallback=self.removeGuideline,
+        )
+        guidelineTab.addButton = vanilla.Button("auto", "+", callback=self.addGuideline)
+        guidelineTab.addAutoPosSizeRules(
+            ["H:|-[list]-|", "H:[addButton]-|", "V:|-[list]-[addButton]-|"]
+        )
+
         self.onLayerChange()
         self.w.createPreviewButton = vanilla.Button(
             "auto", "Create preview master", callback=self.createPreviewMaster
         )
-        # self.w.closeButton = vanilla.Button("auto", "Close", callback=self.close)
         rules = [
             "H:|-[instanceSelector]-|",
             "H:|-[tabs]-|",
@@ -230,11 +359,27 @@ class PendotDesigner:
         Glyphs.addCallback(self.createLayerPreview, "GSUpdateInterface")
         self.createLayerPreview()
 
+    def removeGuideline(self, sender=None):
+        selectedIndexes = self.w.tabs[2].list.getSelectedIndexes()
+        if not len(selectedIndexes):
+            return
+        selectedIndex = selectedIndexes[0]
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        items = instance.userData[KEY + ".guidelines"]
+        del items[selectedIndex]
+        self.reloadGuidelines()
+
+    def addGuideline(self, sender=None):
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        items = instance.userData[KEY + ".guidelines"]
+        items.append({"height": "0", "thickness": 10})
+        self.reloadGuidelines()
+
     def finish(self, sender=None):
         Glyphs.removeCallback(self.onLayerChange)
         Glyphs.removeCallback(self.createLayerPreview)
         del self.w
-    
+
     def _is_valid_source(self, layer):
         if layer.name.endswith(" dotted"):
             return False
@@ -249,7 +394,9 @@ class PendotDesigner:
             return
         alternate_layers = ["<Default>"]
         if layers[0]:
-            alternate_layers.extend([l.name for l in layers[0].parent.layers if self._is_valid_source(l)])
+            alternate_layers.extend(
+                [l.name for l in layers[0].parent.layers if self._is_valid_source(l)]
+            )
             self.w.tabs[0].contourSource.defaultwidget.setItems(alternate_layers)
             self.w.tabs[0].contourSource.overridewidget.setItems(alternate_layers)
             for tab in (self.w.tabs[0], self.w.tabs[1]):
@@ -258,14 +405,51 @@ class PendotDesigner:
                 )
         self.reloadValues()
 
+    def editGuidelines(self, sender):
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        instance.userData[KEY + ".guidelines"] = [
+            {"height": item["height"], "thickness": item["thickness"]}
+            for item in self.w.tabs[2].list.get()
+        ]
+        self.createLayerPreview()
+
     def updateDots(self, sender=None):
         if self.w.tabs.get() == 0:
             self.createLayerPreview()
 
     def reloadValues(self, sender=None):
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        if KEY + ".mode" in instance.userData:
+            if instance.userData[KEY + ".mode"] == "dotter":
+                self.w.tabs.set(0)
+            elif instance.userData[KEY + ".mode"] == "stroker":
+                self.w.tabs.set(1)
         for reloader in self.widget_reloaders:
             reloader()
-    
+        self.reloadGuidelines()
+
+    def reloadGuidelines(self):
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        if (
+            KEY + ".guidelines" not in instance.userData
+            or not instance.userData[KEY + ".guidelines"]
+        ):
+            instance.userData[KEY + ".guidelines"] = [
+                {"height": "Descender", "thickness": 10},
+                {"height": "x-Height", "thickness": 10},
+                {"height": "Cap Height", "thickness": 10},
+                {"height": "Ascender", "thickness": 10},
+            ]
+        items = instance.userData[KEY + ".guidelines"]
+        # Make a deep mutable copy of this
+        self.w.tabs[2].list.set(
+            [
+                {"height": item["height"], "thickness": item["thickness"]}
+                for item in items
+            ]
+        )
+        self.createLayerPreview()
+
     @property
     def mode(self):
         tabIndex = self.w.tabs.get()
@@ -273,12 +457,19 @@ class PendotDesigner:
             return "dotter"
         elif tabIndex == 1:
             return "stroker"
+        else:
+            return "guides"
 
     def createLayerPreview(self, sender=None):
-        if self.idempotence or not Glyphs.font.selectedLayers:
-            return
+        # Store the current mode inside the instance object
+        instance = self.selectedInstance or Glyphs.font.instances[0]
+        instance.userData[KEY + ".mode"] = self.mode
+
         self.idempotence = True
-        layername = self.mode.replace("er", "ed")
+        if self.mode == "guides":
+            layername = "guides"
+        else:
+            layername = self.mode.replace("er", "ed")
 
         font = Glyphs.font
         for layer in Glyphs.font.selectedLayers:
@@ -304,9 +495,16 @@ class PendotDesigner:
                     try:
                         destination_layer.parent.beginUndo()
                         if layername == "dotted":
-                            destination_layer.shapes = doDotter(layer, instance, component=False)
+                            destination_layer.shapes = doDotter(
+                                layer, instance, component=False
+                            )
                         elif layername == "stroked":
                             destination_layer.shapes = doStroker(layer, instance)
+                        else:
+                            destination_layer.shapes = (
+                                layer.copyDecomposedLayer().shapes
+                            )
+                            add_guidelines_to_layer(destination_layer, instance)
                         destination_layer.parent.endUndo()
                         Glyphs.redraw()
                     except Exception as e:
@@ -342,7 +540,6 @@ class PendotDesigner:
             preview_master.descender = master.descender
             preview_master.xHeight = master.xHeight
 
-
         del Glyphs.font.glyphs["_dot"]
         addComponentGlyph(Glyphs.font, instance)
 
@@ -368,6 +565,7 @@ class PendotDesigner:
             else:
                 preview_layer.shapes = doStroker(layer, instance)
         Glyphs.redraw()
+
 
 if Glyphs.font:
     if not hasattr(GSApplication, "_pendotdesigner"):
