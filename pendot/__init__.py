2b149b871d0e02a159473a3fe1e9b2171633f58d
Simon Cozens <simon@simon-cozens.org>
2025-06-11 13:44:18 +0100

    More defensive programming

--- pendot/__init__.py
+++ pendot/__init__.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+import sys
 from typing import List, Optional
 
 from pendot.constants import KEY, PREVIEW_MASTER_NAME, QUICK_PREVIEW_LAYER_NAME
@@ -58,12 +59,50 @@ def create_effects(
     return effects
 
 
-def transform_font(font: GSFont, effects: List[Effect]):
+def transform_font(
+    font: GSFont, effects: List[Effect], instance: Optional[GSInstance] = None
+):
     results = {}
     font.masters = [m for m in font.masters if m.name != PREVIEW_MASTER_NAME]
+    if len(font.masters) > 1:
+        if instance is None and len(font.instances) == 1:
+            instance = font.instances[0]
+        if instance is None:
+            print("No instance provided, using first master as relevant master.")
+            relevant_master = font.masters[0]
+        else:
+            relevant_masters = [m for m in font.masters if m.axes == instance.axes]
+            if len(relevant_masters) > 1:
+                print(
+                    "Multiple masters found for instance, using first one as relevant master."
+                )
+            elif len(relevant_masters) == 0:
+                print(
+                    f"Couldn't find master for instance {instance}, check your axis values."
+                )
+                sys.exit(1)
+            relevant_master = relevant_masters[0]
+    else:
+        relevant_master = font.masters[0]
+
     for glyph in progress(font.glyphs):
-        for layer in glyph.layers:
-            if layer.layerId == layer.associatedMasterId:
+        relevant_layers = [
+            layer for layer in glyph.layers if layer.layerId == relevant_master.id
+        ]
+        if not relevant_layers:
+            logger.warning(
+                f"Glyph {glyph.name} has no layer for master {relevant_master.name}, skipping."
+            )
+            continue
+        if len(relevant_layers) > 1:
+            logger.warning(
+                f"Glyph {glyph.name} has multiple layers for master {relevant_master.name}, don't know which to use."
+            )
+            sys.exit(1)
+
+        for layer in relevant_layers:
+            if layer.layerId == relevant_master.id:
+                print("Processing layer ", layer)
                 results[layer] = transform_layer(layer, effects)
     for layer, shapes in results.items():
         if shapes:
